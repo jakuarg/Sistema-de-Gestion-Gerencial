f1a2d873d69b81fd165829dd6b18972c9350c2b3
Juanpi2601 <juanpablofortuny89@gmail.com>
2020-11-17 23:23:56 -0300

    Arranco con Consultorio.cpp

---
+++ bin/modules/consultorio.cpp
@@ -0,0 +1,28 @@
+/*
+  ***********************************
+  *** Trabajo Practico Grupal No2 ***
+  **  Grupo N 					   **
+  *   Veterinaria					*
+  ***********************************
+  **********CONSULTORIO**************
+  ***********************************
+*/
+//DECLARACIONES DE LIBRERIAS
+#include<stdio.h>//Funciones de entrada
+#include<stdlib.h>//Funciones system
+//PARA HABILITAR EL USO DE CARACTERES ESPECIALES
+#include <wchar.h>
+#include <locale.h>
+//PARA HABILITAR FUNCIONES DE TEXTO
+#include <ctype.h>
+#include <string.h>
+//PARA HABILITAR FUNCIONES ESPECIALES DE MANIPULACION DE VENTANA,ETC
+#include <windows.h>
+
+main()
+{
+
+
+
+
+}
