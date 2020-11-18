9d74a1312ceb62679c5ec4a32038d620200b051f
David <jacasdavid@gmail.com>
2020-11-17 21:25:07 -0300

    Proceso de creacion de autenticacion de usuario

--- bin/modules/administracion.cpp
+++ bin/modules/administracion.cpp
@@ -8,12 +8,15 @@
   ***********************************
 */
 //DECLARACIONES DE LIBRERIAS
-#include<stdio.h>
-#include<stdlib.h>
+#include<stdio.h>//Funciones de entrada
+#include<stdlib.h>//Funciones system
 //PARA HABILITAR EL USO DE CARACTERES ESPECIALES
 #include <wchar.h>
 #include <locale.h>
-
+//PARA HABILITAR FUNCIONES DE TEXTO
+#include <ctype.h>
+#include <string.h>
+//PARA HABILITAR FUNCIONES ESPECIALES DE MANIPULACION DE VENTANA,ETC
 #include <windows.h>
 
 //DEFINICIONES DE LA LONGITUD DEL USUARIO Y CONTRASENIA
@@ -28,17 +31,18 @@ struct auth //ESTRUCTURA DEL USUARIO
 	
 };
 
-//FUNCIONES
-int val_user(userlen usuario,passlen contrasenia,int usorpass);//int usorpass es la bandera para ver si se manda un usuario o una contrasenia
+//FUNCIONES de validacion
+int val_user(userlen usuario,passlen contrasenia);
 int val_in(int entrada,int lim_min, int lim_max);
+//FUNCIONES de registro
+void Reg_Veterinario();
+
 main()
 {
 	setlocale(LC_ALL, "SPANISH");
 	system("CLS");
-	
-	//ALTA
-	FILE*archauth;
-	archauth=fopen("usuarios.dat","w+b");
+
+
 	//SECCION DE DECLARACIONES 
 	int error,op_1;
 	//
@@ -66,22 +70,32 @@ main()
 	switch(op_1)
 	{
 		case 1:
+			Reg_Veterinario();
 			break;//Reg_Vet();
 	}
-	fclose(archauth);
 }
 
-int val_user(userlen usuario,passlen contrasenia,int usorpass)
+int val_user(userlen usuario,passlen contrasenia)//Verifica si el usuario y contra es correcta
 {
-	switch(usorpass)
+	//CONTADORES
+	int lmay=0;//lETRAS MAYUSCULAS
+	int cuser=0;//CONTADOR USUARIO
+	//VALIDACION DE USUARIO
+	while(strlen(usuario)>=6 && strlen(usuario)<=10)//Verifica si el usuario tiene como minimo 6 y maximo 10 caracteres
 	{
-		case 1://usuario
-			break;
-		case 2://contrasenia
-			 break; 
+		while(islower(usuario[0])!=0)//verifica si el primer caracter es minuscula
+		{
+			while(lmay<2 || strlen(usuario)>cuser)//verifica si tiene al menos 2 mayusculas
+			{
+				if(isupper(usuario[cuser])!=0)
+				{
+					;
+				}
+			}
+		}
 	}
 }
-int val_in(int entrada,int lim_min, int lim_max)
+int val_in(int entrada,int lim_min, int lim_max)//Ingresa una entrada y verifica si esta entre los limites
 {
 	if(lim_max>lim_min)
 	{
@@ -91,4 +105,42 @@ int val_in(int entrada,int lim_min, int lim_max)
 		}else return 0;
 	}else printf("\nEl limite minimo no puede ser mayor al maximo.");
 	
+}
+
+void Reg_Veterinario()
+{
+	system("CLS");
+	//ALTA
+	FILE*arch_admin;
+	arch_admin=fopen("USERSinf.dat","a+b");
+	if(arch_admin==NULL)
+	{
+		system("CLS");
+		printf("\n\n SE PRODUJO UN ERROR AL INTENTAR ABRIR EL ARCHIVO");
+		system("PAUSE");
+		exit(1);
+	}
+	//FIN DE ALTA
+
+	//Llamada al registro
+	auth rv; //rv=registroveterinario
+	//Declaracion de Variables
+	int error;
+	//Inicio interfaz de ingresado
+	printf("Registrar Veterinario");
+	printf("\n=======================\n");
+	error=1;
+	while(error==1)
+	{
+		_flushall();
+		printf("Usuario:    "); gets(rv.user);
+		printf("\nContraseña: "); gets(rv.password);
+
+		if(val_user(rv.user,rv.password)==0)
+		{
+			printf("Ingreso fallido. Nombre o Contraseña no valido.");
+		}else error=0;
+	}
+	//BAJA
+	fclose(arch_admin);
 }
