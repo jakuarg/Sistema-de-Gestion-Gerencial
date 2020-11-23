e26943b4fc64050b83d018daa546be881f62d581
Lucas Patricio Juarez <Lucas.Juarez@alu.frt.utn.edu.ar>
2020-11-23 20:56:03 -0300

    Implementacion en el menu recepcion.exe

--- bin/modules/recepcion.cpp
+++ bin/modules/recepcion.cpp
@@ -18,7 +18,7 @@
 #include <string.h>
 //PARA HABILITAR FUNCIONES ESPECIALES DE MANIPULACION DE VENTANA,ETC
 #include <windows.h>
-
+#include "functions/admin.h"
 /******** Estructuras *******/
 
 struct fecha{
@@ -64,11 +64,11 @@ main()
     system ("colorF0");
    int menu; // Variable de ingreso al menu
     
-    FILE *ArchTurno = fopen("Turno.dat","r+b");
+    FILE *ArchTurno = fopen("bin/modules/Turno.dat","r+b");
     if(ArchTurno == NULL){
           
           fclose(ArchTurno);
-          ArchTurno = fopen("Turno.dat","w+b");
+          ArchTurno = fopen("bin/modules/Turno.dat","w+b");
 
      } 
      
